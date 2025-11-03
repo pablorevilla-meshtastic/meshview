5ca3b472a66895569db188abf2417cc357a20947
Joel Krauska <jkrauska@gmail.com>
2025-11-03 13:26:44 -0800

    Store UTC int time in DB (#81)
    * use UTC int time

--- alembic/versions/1717fa5c6545_add_example_table.py
+++ alembic/versions/1717fa5c6545_add_example_table.py
@@ -6,7 +6,7 @@ Create Date: 2025-10-26 20:59:04.347066
 
 """
 
-from collections.abc import Sequence
+from typing import Sequence, Union
 
 import sqlalchemy as sa
 
@@ -14,9 +14,9 @@ from alembic import op
 
 # revision identifiers, used by Alembic.
 revision: str = '1717fa5c6545'
-down_revision: str | None = 'c88468b7ab0b'
-branch_labels: str | Sequence[str] | None = None
-depends_on: str | Sequence[str] | None = None
+down_revision: Union[str, None] = 'c88468b7ab0b'
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
 
 
 def upgrade() -> None:
